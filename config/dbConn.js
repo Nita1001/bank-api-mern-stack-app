6b8b0c51e4d74e34e7c12dea75504d949600329a
Nita1001 <devinedevops@gmail.com>
2023-03-25 00:55:29 +0300

    cyclic

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+        const uri = process.env.CONNECTION_STRING;
+        const connect = await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
