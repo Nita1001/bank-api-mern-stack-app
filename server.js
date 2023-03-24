65ccc05b5dacfc238f79327518f4842fd544a5d3
Nita1001 <devinedevops@gmail.com>
2023-03-24 16:12:09 +0300

    creating users

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config/config.env' });
+const connectDb = require('./config/dbConn.js')
+const errorHandler = require('./middleware/errorHandler.js')
 
 const app = express();
 
+// Load environment config.env 
+dotenv.config({ path: './config/config.env' });
+
+// Connect to MongoDB database
+connectDb();
+
+// Parse request body
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => res.send('Server running'));
+app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5001;
+// Error handling middleware
+app.use(errorHandler);
 
+// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
