43f96a31958e148d0a4596e5803a4d5adbf9a35c
Nita1001 <devinedevops@gmail.com>
2023-03-24 13:31:11 +0300

    userSchema name email

---
+++ models/userSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+
+})
+
+module.exports = mongoose.model('User', userSchema);
