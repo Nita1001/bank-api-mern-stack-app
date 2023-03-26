867df696110e7ff3a4d20f6020b891e57587d36f
Nita1001 <devinedevops@gmail.com>
2023-03-26 04:48:57 +0300

    usersList

---
+++ client/src/components/UsersList.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { getUsers, createNewUser, updateUser } from "../api/bankAPI";
+
+import "./styles/UsersList.style.css";
+import TransferFunds from "./TransferFunds";
+
+const UsersList = () => {
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [currentAction, setCurrentAction] = useState("");
+
+    useEffect(() => {
+        getUsers().then((data) => setUsers(data));
+    }, []);
+
+    const handleTransferFunds = () => {
+        setCurrentAction("transfer");
+    };
+
+    const handleDepositCash = () => {
+        setCurrentAction("deposit");
+    };
+
+    const handleWithdrawCash = () => {
+        setCurrentAction("withdraw");
+    };
+
+    const handleTransferTarget = (targetUser) => {
+        console.log(
+            `Transferred funds from ${selectedUser.firstName} ${selectedUser.lastName} to ${targetUser.firstName} ${targetUser.lastName}`
+        );
+        setCurrentAction("");
+    };
+
+    const handleDepositAmount = (event) => {
+        console.log(
+            `Deposited ${event.target.value} to ${selectedUser.firstName} ${selectedUser.lastName}`
+        );
+    };
+
+    const handleWithdrawAmount = (event) => {
+        console.log(
+            `Withdrew ${event.target.value} from ${selectedUser.firstName} ${selectedUser.lastName}`
+        );
+    };
+
+    return (
+        <div className="App">
+            <h1>Users</h1>
+
+            <ol className="listed">
+                {users.map((user) => (
+                    <li key={user._id} onClick={() => setSelectedUser(user)}>
+                        <a href="#">{user.firstName}</a>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
+};
+
+export default UsersList;
